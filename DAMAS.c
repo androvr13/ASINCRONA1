4581da37e55a376af61cb7647172a9f3e6a00121
andro <andro.vergara@iteso.mx>
2026-02-16 21:20:47 -0600

    version final

--- DAMAS.c
+++ DAMAS.c
@@ -31,7 +31,7 @@ void imprimir(const char *tablero){
         for (j= 0; j < SIZE; j++)
         {
             acomodar = i * SIZE + j; //acomoda las filas y las columnas 
-            printf("| %c ",tablero[acomodar]);
+            printf("| %c ",*(tablero+acomodar));
         }
         printf("|\n");
 }
@@ -40,17 +40,23 @@ void imprimir(const char *tablero){
 int movimiento(char *t, char jugador){
     int fil, col; //posición actual
     int nuevaFil, nuevaCol; //posición destino
-    int intentos= 0; //contador de intentos para evitar bucles infinitos
+    int intentos= 0; //contador de intentos para evitar bucles infinitos (máximo 100)
+    int d; //contador para recorrer direcciones de movimiento
+    int direcciones[2]; //direcciones de movimiento para cada jugador
+
+    direcciones[0]=1; //baja una fila
+    direcciones[1]=-1; //sube una fila
+
 
     while(intentos<100){
         fil=rand()%SIZE; //genera fila aleatoria entre 0 y 7
         col=rand()%SIZE;
 
     if(*(t+fil*SIZE+col)==jugador){ //tablero[fil][col], verifica que la posición seleccionada tenga una ficha del jugador
-        nuevaFil=(jugador=='X') ? fil+1 : fil-1; //decide la dirección del movimiento: las fichas X avanzan hacia abajo (fil+1) y las O hacia arriba (fil-1)
+        for(d=0; d<2; d++){ //intenta mover en ambas direcciones (hacia arriba y hacia abajo) *cumple requisito diagonal
+            nuevaFil = fil + direcciones[d]; //calcula la nueva fila según la dirección
 
     if(nuevaFil>=0 && nuevaFil<SIZE){ //verifica que nunca se salga del tablero
-
 
         //LADO IZQUIERDO
         nuevaCol=col-1; //intenta mover a la izquierda
@@ -69,9 +75,10 @@ int movimiento(char *t, char jugador){
             }
         }
     }
+}
     intentos++;
-    }
-    return 0; //si no se logra hacer un movimiento válido después de 100 intentos
+}
+return 0; //si no se logra hacer un movimiento válido después de 100 intentos
 }
 
 int main(){
@@ -87,12 +94,14 @@ int main(){
     for(turno=1; turno<=10; turno++){ //10 turnos para cada jugador
         printf("\nTurno %d - X(ENTER)\n", turno);
         while((enter=getchar()) != '\n'); //espera a que el usuario presione ENTER para continuar
-        movimiento(ptr, 'X');
+        if(!movimiento(ptr, 'X'))
+            printf("Sin movimientos, se pasa turno.\n");
         imprimir(ptr);
 
         printf("\nTurno %d - O(ENTER)\n", turno);
         while((enter=getchar()) != '\n');
-        movimiento(ptr, 'O');
+        if(!movimiento(ptr, 'O'))
+            printf("Sin movimientos, se pasa turno.\n");
         imprimir(ptr);
     }
     printf("\nJuego terminado\n");
